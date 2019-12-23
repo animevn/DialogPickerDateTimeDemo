88e63820289477989b27078df5136b6bdd659a00
animevn <haanhth21@gmail.com>
2019-12-23 18:14:13 +0700

    now picker will survive rotation view

--- app/src/main/java/com/haanhgs/app/dialogpickerdatetimedemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/dialogpickerdatetimedemo/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import butterknife.BindView;
 import butterknife.ButterKnife;
